1970d141546f045f494d06836d96275b08c47363
Kevin Tran <kevin.tran.mk@gmail.com>
2019-03-16 00:12:18 -0400

    merge player and dialogbox

---
+++ Player.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+export (int) var speed  # How fast the player will move (pixels/sec).
+var screensize  # Size of the game window.
+
+
+func _ready():
+	screensize = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2() # The player's movement vector.
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+		
+func start(pos):
+	position = pos
